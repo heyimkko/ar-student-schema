d8dddb8fe2dc9c94c30aaf1658eb24fec09a5e3d
Apprentice <apprentice@dbc29.local>
2013-03-07 10:53:50 -0800

    Completed first migration

--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -3,8 +3,16 @@ require_relative '../config'
 # this is where you should use an ActiveRecord migration to 
 
 class CreateStudents < ActiveRecord::Migration
-  def change(name, options)
+  def change
     # HINT: checkout ActiveRecord::Migration.create_table
-    create_table(name, options)
+    create_table :students do |t|
+      t.integer :id
+      t.string  :first_name
+      t.string  :last_name
+      t.string  :gender
+      t.date    :birthday
+      t.string  :email
+      t.string  :phone
+    end
   end
 end
